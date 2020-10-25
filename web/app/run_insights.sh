b605238b48c3649e0c4699a2e44e1ad6e58a0b75
Johannes Christler <chrislter.johannes@gmail.com>
2020-10-25 13:39:46 +0100

    custom swiftlint yml

--- web/app/run_insights.sh
+++ web/app/run_insights.sh
@@ -1,16 +1,9 @@
 BB_REPO=$1
 COMMIT_ID=$2
 
-echo $BB_TOKEN
-echo $BB_BASE_URL
-echo $BB_PROJECT
-echo $BB_REPORT_SLUG
-echo $BB_REPO
-echo $COMMIT_ID
-
 echo "Cloning Repo"
 curl -L \
--H "Authorization: Bearer $$BB_TOKEN" \
+-H "Authorization: Bearer $BB_TOKEN" \
 "$BB_BASE_URL/rest/api/1.0/projects/$BB_PROJECT/repos/$BB_REPO/archive?filename=sourceFiles.zip&at=$COMMIT_ID" \
 "-o$COMMIT_ID.zip"
 echo "Dome"
@@ -22,6 +15,7 @@ echo "Done"
 # Run the analysis and parse the output
 echo "Running Swift Lint"
 touch $COMMIT_ID.out
+cp .swiftlint.yml $COMMIT_ID/.swiftlint.yml
 cd $COMMIT_ID
 swiftlint >> ../$COMMIT_ID.out
 cd ..
@@ -35,7 +29,7 @@ echo "Done"
 echo "Creating insight report"
 curl \
 -H "Content-type: application/json" \
--H "Authorization: Bearer $$BB_TOKEN" \
+-H "Authorization: Bearer $BB_TOKEN" \
 -X PUT \
 -d @report_$COMMIT_ID.json \
 "$BB_BASE_URL/rest/insights/latest/projects/$BB_PROJECT/repos/$BB_REPO/commits/$COMMIT_ID/reports/$BB_REPORT_SLUG"
@@ -44,7 +38,7 @@ echo "Done"
 # Delete old annotations from the report (they may not exist but it is better to be safe)
 echo "Deleting any existing annotations"
 curl \
--H "Authorization: Bearer $$BB_TOKEN" \
+-H "Authorization: Bearer $BB_TOKEN" \
 -H "X-Atlassian-Token: no-check" \
 -X DELETE \
 "$BB_BASE_URL/rest/insights/latest/projects/$BB_PROJECT/repos/$BB_REPO/commits/$COMMIT_ID/reports/$BB_REPORT_SLUG/annotations"
@@ -54,7 +48,7 @@ echo "Done"
 echo "Adding annotations to report"
 curl \
 -H "Content-type: application/json" \
--H "Authorization: Bearer $$BB_TOKEN" \
+-H "Authorization: Bearer $BB_TOKEN" \
 -X POST \
 -d @annotations_$COMMIT_ID.json \
 "$BB_BASE_URL/rest/insights/latest/projects/$BB_PROJECT/repos/$BB_REPO/commits/$COMMIT_ID/reports/$BB_REPORT_SLUG/annotations"
